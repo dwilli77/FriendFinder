fef7c9584f1234203b6a9983afdf241fce4aade7
Derek W <41767875+dwilli77@users.noreply.github.com>
2018-12-05 21:49:53 -0500

    get routes and friends built

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+let friends = [
+    {
+        "name": "The Grinch",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Sameer",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Sarah",
+        "photo": "",
+        "scores": []
+    }
+]
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+let friends = require('../data/friends');
+
+module.exports = function(app) {
+
+    app.get('/api/friends', function(req,res){
+        res.json(friends);
+    });
+
+    app.post('/api/tables', function(req,res) {
+        friends.push(req.body);
+        let userScoresArr = req.body.scores;
+        //send back user's match
+    })
+}
